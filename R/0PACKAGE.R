7a3c29e5488e1d7c6b18ffd074ebbfac823e9b7f
Tingting Zhan <tingtingzhan@gmail.com>
2026-08-05 12:49:12 -0400

    tzh

--- R/0PACKAGE.R
+++ R/0PACKAGE.R
@@ -5,11 +5,8 @@
 # \doi{10.1016/0197-2456(89)90015-9}
 
 
-#' @note
-#' Help files of individual functions are intentionally suppressed in the `pdf` manual.
-#' Users are encouraged to get started with 
-#' 
-#' `vignette('intro', package = 'VisualizeSimon2Stage')`
+#' @references
+#' \url{https://rpubs.com/tingtingzhan/VisualizeSimon2Stage}
 #' 
 #' @import methods
 '_PACKAGE'
